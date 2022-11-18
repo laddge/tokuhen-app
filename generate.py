421a94cb0fc99ace88086727cd71d494843c2831
Laddge <dev.laddge@gmail.com>
2022-11-18 08:52:51 +0000

    ディレクトリ構造を変更

--- generate.py
+++ generate.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import json
 import toml
+import transit
 
 
 def gen():
@@ -10,10 +11,12 @@ def gen():
     elif os.path.exists("dist"):
         os.remove("dist")
     os.mkdir("dist")
-    with open("timetable.toml") as f:
+    with open("src/timetable.toml") as f:
         tt = toml.load(f)
     with open("dist/timetable.json", "w") as f:
         json.dump(tt, f)
+    with open("dist/transit.json", "w") as f:
+        json.dump(transit.main(), f)
 
 
 if __name__ == "__main__":
